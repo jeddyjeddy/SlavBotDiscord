a795d9a222858575e4f9436d4c762c7ee10c016b
Daniyal Khan <daniyalkhan2000@users.noreply.github.com>
2018-07-14 18:01:41 +0400

    Website Update

--- commands/textshit/share.js
+++ commands/textshit/share.js
@@ -8,7 +8,7 @@ class ShareCommand extends command.Command
             name: "share",
             group: "util",
             memberName: "share",
-            description: "Gives the DBL link for Slav Bot. Share Slav Bot on other servers using this link.",
+            description: "Gives the link to the Slav Bot website. Share Slav Bot on other servers using this link.",
             examples: ["`!share`"]
         });
     }
@@ -16,7 +16,7 @@ class ShareCommand extends command.Command
     async run(message, args)
     {
         message.channel.startTyping();
-        message.reply("https://discordbots.org/bot/319533843482673152").catch(error => console.log("Send Error - " + error));
+        message.reply("https://merriemweebster.github.io/SlavBot").catch(error => console.log("Send Error - " + error));
         message.channel.stopTyping();
     }
 }
