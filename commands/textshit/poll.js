cc2a9ae6ed420f826cba879ace1f0dbd91bf3f3d
Daniyal Khan <daniyalkhan2000@users.noreply.github.com>
2018-07-19 19:33:12 +0400

    Fix

--- commands/textshit/poll.js
+++ commands/textshit/poll.js
@@ -431,10 +431,6 @@ class PollCommand extends command.Command
                     var selectedOptionIndex = selectedOption - 1;
                     var hasVoted = false;
 
-                    console.log(selectedOption)
-                    console.log(selectedOptionIndex)
-                    console.log(allPolls[guildIndex].polls[pollIndex].poll.options)
-
                     if(allPolls[guildIndex].polls[pollIndex].poll.options[selectedOptionIndex].users != null)
                     {
                         if(allPolls[guildIndex].polls[pollIndex].poll.options[selectedOptionIndex].users.length > 0)
