0b1b5bfa5db022dcddc2de137435811de62ad622
Daniyal Khan <daniyalkhan2000@users.noreply.github.com>
2018-07-21 20:54:20 +0400

    Fixes

--- index.js
+++ index.js
@@ -181,8 +181,7 @@ var commandCounterChange = (userID) => {
                 {
                     if(userCommandUsage[i].requestsSent < 3)
                     {
-                        console.log(dbl.isWeekend())
-                        dbl.isWeekend(weekend => {
+                        dbl.isWeekend().then(weekend => {
                             if (weekend)
                             {
                                 if(userCommandUsage[i].weekendUsesCheck < userCommandUsage[i].uses)
@@ -223,7 +222,7 @@ var commandCounterChange = (userID) => {
                             }
                             else
                             {
-                                dbl.isWeekend(weekend => {
+                                dbl.isWeekend().then(weekend => {
                                     if (weekend)
                                     {
                                         if(userCommandUsage[i].weekendUsesCheck < userCommandUsage[i].uses)
