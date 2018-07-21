40e208d088369ee08992bf91d9f06be135022c78
Daniyal Khan <daniyalkhan2000@users.noreply.github.com>
2018-07-21 18:45:25 +0400

    Fix

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ const app = express()
 app.use(bodyParser.json())
 var port = (process.env.PORT || 5000)
 
-app.post("/", function (req, res) {
+app.post('/', function (req, res) {
     const AUTH_TOKEN = process.env.VOTE_AUTH_TOKEN
     console.log("Received")
     if (req.headers['Authorization'] !== AUTH_TOKEN) {
