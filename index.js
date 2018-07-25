995a491682c5b33992b8880b82f0ea0498595d13
Daniyal Khan <daniyalkhan2000@users.noreply.github.com>
2018-07-26 01:11:22 +0400

    Fix

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ var getUserCommandCounter = (userID) => {
 }
 
 var commandCounterChange = (userID) => {
-    if(!signedIntoFirebase)
+    if(!signedIntoFirebase || userCommandUsage == [{key: "Key", uses: 0, requestsSent: 0, weekendUsesCheck: 100, usesCheck: 250}])
     {
         commandCounterChange(userID);
         return;
