c2e2cf5f23a925ce48e297e07c0f5cbb1f8029ac
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-07-26 11:04:55 +0400

    Kanye Command

---
+++ commands/imageshit/kanye.js
@@ -0,0 +1,226 @@
+const command = require("discord.js-commando");
+const Jimp = require("jimp");
+const shortid = require("shortid");
+const fs = require('fs');
+var resultHandler = function(err) { 
+    if(err) {
+       console.log("unlink failed", err);
+    } else {
+       console.log("file deleted");
+    }
+}
+var CommandCounter = require("../../index.js")
+
+class KanyeCommand extends command.Command
+ {
+    constructor(client)
+    {
+        super(client, {
+            name: "kanye",
+            group: "imageshit",
+            memberName: "kanye",
+            description: "Have Kanye hold your image. This command takes the last image uploaded, your avatar or the avatar of the user you have mentioned after the command.",
+            examples: ["`!kanye`", "`!kanye avatar`", "`!kanye @User`"]
+        });
+    }
+
+    async run(message, args)
+    {
+        message.channel.startTyping();
+        CommandCounter.addCommandCounter(message.author.id)
+
+        var otherUser = false;
+        var userID = "";
+        var commandPrefix= "!"
+        if(message.guild != null)
+        {
+            commandPrefix = message.guild.commandPrefix
+        }
+        if(args.length > 0)
+        {
+            console.log("args are present");
+            var getUser = false;
+            for(var i = 0; i < args.length; i++)
+            {
+                if(getUser)
+                {
+                    if(args[i].toString() == ">")
+                    {
+                        i = args.length;
+                        otherUser = true;
+                    }
+                    else
+                    {
+                        if(args[i].toString() != "@" && !isNaN(args[i].toString()))
+                        {
+                            userID = userID + args[i].toString();
+                        }
+                    }
+                }
+                else
+                {
+                    if(args[i].toString() == "<")
+                    {
+                         getUser = true;
+                    } 
+                }
+            }
+        }
+        
+        var url = "";
+        console.log(url);
+
+        if(args.toString().toLowerCase() != "avatar" && !otherUser)
+        {
+            message.channel.fetchMessages({ around: message.id })
+            .then(messages => {
+                var messageID = "";
+                messages.filter(msg => {
+                    if(msg.attachments.first() != undefined)
+                    {
+                        if(msg.attachments.last().height > 0)
+                        {
+                            if(messageID == "")
+                            {
+                                messageID = msg.id;
+                                url = msg.attachments.first().url;
+                            }
+                        }
+                    }
+                });
+
+                if(messageID == "")
+                {
+                    message.reply("no image found, , use `" + commandPrefix + "help kanye` for help.").catch(error => console.log("Send Error - " + error));
+                    message.channel.stopTyping();
+                    return;
+                }
+                message.reply("***taking image***").catch(error => console.log("Send Error - " + error));
+                Jimp.read("Kanye.png").then(function (KanyeImage) {
+                    console.log("got image");
+                    Jimp.read(url).then(function (userImage) {
+                        console.log("got avatar");
+                        
+                        var x = 223
+                        var y = 500
+
+                        userImage.cover(680, 560);
+                        userImage.rotate(2);
+
+                        var blank = new Jimp(KanyeImage.bitmap.width, KanyeImage.bitmap.height);
+                        blank.composite(userImage, x, y)        
+        
+                        var mergedImage = blank.composite(KanyeImage, 0, 0);
+                        var file = shortid.generate() + ".png"
+                        mergedImage.write(file, function(error){
+                            if(error) throw error;
+                            console.log("got merged image");
+                            console.log(file);
+                            message.channel.send("**Thank you Kanye, very cool!**", {
+                                files: [file]
+                            }).then(function(){
+                                message.channel.stopTyping();
+                                setTimeout(function(){
+                                    fs.unlink(file, resultHandler);
+                                    console.log("Deleted " + file);
+                                }, 10000);
+                            }).catch(function (err) {
+                                message.reply("Error - " + err.message).catch(error => console.log("Send Error - " + error));
+                                console.log(err.message);
+                                message.channel.stopTyping();
+                                setTimeout(function(){
+                                    fs.unlink(file, resultHandler);
+                                    console.log("Deleted " + file);
+                                }, 10000);
+                            });
+                            console.log("Message Sent");
+                        });
+                    }).catch(function (err) {
+                        message.reply("Error - " + err.message).catch(error => console.log("Send Error - " + error));
+                        console.log(err.message);
+                        message.channel.stopTyping();
+                    });
+                }).catch(function (err) {
+                    console.log(err.message);
+                    message.channel.stopTyping();
+                });
+            }).catch(function (err) {
+                message.reply("Error - " + err.message).catch(error => console.log("Send Error - " + error));
+                console.log(err.message);
+                message.channel.stopTyping();
+            });
+        }
+        else if(args.toString().toLowerCase() == "avatar" || otherUser)
+        {
+            if(otherUser)
+            {
+                console.log("other Kanye");
+                console.log(userID);
+    
+                message.channel.client.fetchUser(userID)
+                 .then(user => {
+                        url = user.avatarURL;
+                 }, rejection => {
+                        console.log(rejection.message);
+                 });
+            }
+            else
+            {
+                console.log("self Kanye");
+                userID = message.author.id;
+                url = message.author.avatarURL;
+            }
+            Jimp.read("Kanye.png").then(function (KanyeImage) {
+                console.log("got image");
+                Jimp.read(url).then(function (userImage) {
+                    console.log("got avatar");
+                    userImage.resize(100, 100);
+    
+                    var x = 223
+                    var y = 500
+
+                    userImage.cover(680, 560);
+                    userImage.rotate(2);
+
+                    var blank = new Jimp(KanyeImage.bitmap.width, KanyeImage.bitmap.height);
+                    blank.composite(userImage, x, y)        
+    
+                    var mergedImage = blank.composite(KanyeImage, 0, 0);
+                    var file = shortid.generate() + ".png"
+                    mergedImage.write(file, function(error){
+                        if(error) throw error;
+                        console.log("got merged image");
+                        console.log(file);
+                        message.channel.send("**Thank you Kanye, very cool!**", {
+                            files: [file]
+                        }).then(function(){
+                            message.channel.stopTyping();
+                            setTimeout(function(){
+                                fs.unlink(file, resultHandler);
+                                console.log("Deleted " + file);
+                            }, 10000);
+                        }).catch(function (err) {
+                            message.reply("Error - " + err.message).catch(error => console.log("Send Error - " + error));
+                            console.log(err.message);
+                            message.channel.stopTyping();
+                            setTimeout(function(){
+                                fs.unlink(file, resultHandler);
+                                console.log("Deleted " + file);
+                            }, 10000);
+                        });
+                        console.log("Message Sent");
+                    });
+                }).catch(function (err) {
+                    message.reply("Error - " + err.message).catch(error => console.log("Send Error - " + error));
+                    console.log(err.message);
+                    message.channel.stopTyping();
+                });
+            }).catch(function (err) {
+                console.log(err.message);
+                message.channel.stopTyping();
+            });
+        }
+    }
+}
+
+module.exports = KanyeCommand;
