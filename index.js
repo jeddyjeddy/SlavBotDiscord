f6073cd7cd714f4c3f104548b23932a136a028db
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-08-16 13:40:22 +0400

    Fix

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ dbl.on('posted', () => {
         url: 'https://bots.ondiscord.xyz/bot-api/bots/' + bot.user.id + "/guilds",
         method: 'POST',
         headers: headers3,
-        body: {"guildCount": bot.guilds.size}
+        form: {"guildCount": bot.guilds.size}
     }
 
     // Start the request
