1e0d7caec6559156db086792cd0dbc004e5054b7
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-08-17 20:03:00 +0400

    Fail Command

---
+++ commands/imageshit/fail.js
@@ -0,0 +1,207 @@
+const command = require("discord.js-commando");
+const Jimp = require("jimp");
+const shortid = require("shortid");
+const fs = require('fs');
+var resultHandler = function(err) { 
+    if(err) {
+       console.log("unlink failed", err);
+    } else {
+       console.log("file deleted");
+    }
+}
+var CommandCounter = require("../../index.js")
+
+class FailCommand extends command.Command
+ {
+    constructor(client)
+    {
+        super(client, {
+            name: "fail",
+            group: "imageshit",
+            memberName: "fail",
+            description: "***I have failed you.*** This command takes the last image uploaded, your avatar or the avatar of the user you have mentioned after the command.",
+            examples: ["`!fail`", "`!fail avatar`", "`!fail @User`"]
+        });
+    }
+
+    async run(message, args)
+    {
+        
+        CommandCounter.addCommandCounter(message.author.id)
+
+        var otherUser = false;
+        var userID = "";
+        var commandPrefix= "!"
+        if(message.guild != null)
+        {
+            commandPrefix = message.guild.commandPrefix
+        }
+        if(args.length > 0)
+        {
+            console.log("args are present");
+            var getUser = false;
+            for(var i = 0; i < args.length; i++)
+            {
+                if(getUser)
+                {
+                    if(args[i].toString() == ">")
+                    {
+                        i = args.length;
+                        otherUser = true;
+                    }
+                    else
+                    {
+                        if(args[i].toString() != "@" && !isNaN(args[i].toString()))
+                        {
+                            userID = userID + args[i].toString();
+                        }
+                    }
+                }
+                else
+                {
+                    if(args[i].toString() == "<")
+                    {
+                         getUser = true;
+                    } 
+                }
+            }
+        }
+        
+        var url = "";
+        console.log(url);
+
+        if(args.toString().toLowerCase() != "avatar" && !otherUser)
+        {
+            message.channel.fetchMessages({ around: message.id })
+            .then(messages => {
+                var messageID = "";
+                messages.filter(msg => {
+                    if(msg.attachments.first() != undefined)
+                    {
+                        if(msg.attachments.last().height > 0)
+                        {
+                            if(messageID == "")
+                            {
+                                messageID = msg.id;
+                                url = msg.attachments.first().url;
+                            }
+                        }
+                    }
+                });
+
+                if(messageID == "")
+                {
+                    message.channel.send("<@" + message.author.id + "> No image found, , use `" + commandPrefix + "help fail` for help.").catch(error => {console.log("Send Error - " + error); });
+                    
+                    return;
+                }
+                message.channel.send("***taking image***").catch(error => {console.log("Send Error - " + error); });
+                Jimp.read("fail.png").then(function (FImage) {
+                    console.log("got image");
+                    Jimp.read(url).then(function (userImage) {
+                        console.log("got avatar");
+                        
+                        var x = 250
+                        var y = 20
+
+                        userImage.cover(160, 240)                         
+                        
+        
+                        var mergedImage = new Jimp(FImage.bitmap.width, FImage.bitmap.height).composite(userImage, x, y ).composite(FImage, 0, 0);
+                        var file = shortid.generate() + ".png"
+                        mergedImage.write(file, function(error){
+                            if(error) { console.log(error); return;};
+                            console.log("got merged image");
+                            console.log(file);
+                            message.channel.send("**I have failed you**", {
+                                files: [file]
+                            }).then(function(){
+                                
+                                fs.unlink(file, resultHandler);
+                            }).catch(function (err) {
+                                message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                                console.log(err.message);
+                                
+                                fs.unlink(file, resultHandler);
+                            });
+                            console.log("Message Sent");
+                        });
+                    }).catch(function (err) {
+                        message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                        console.log(err.message);
+                        
+                    });
+                }).catch(function (err) {
+                    console.log(err.message);
+                    
+                });
+            }).catch(function (err) {
+                message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                console.log(err.message);
+                
+            });
+        }
+        else if(args.toString().toLowerCase() == "avatar" || otherUser)
+        {
+            if(otherUser)
+            {
+                console.log("other Fail");
+                console.log(userID);
+    
+                message.channel.client.fetchUser(userID)
+                 .then(user => {
+                        url = user.avatarURL;
+                 }, rejection => {
+                        console.log(rejection.message);
+                 });
+            }
+            else
+            {
+                console.log("self Fail");
+                userID = message.author.id;
+                url = message.author.avatarURL;
+            }
+            Jimp.read("fail.png").then(function (FImage) {
+                console.log("got image");
+                Jimp.read(url).then(function (userImage) {
+                    console.log("got avatar");
+                    
+                    var x = 250
+                    var y = 20
+
+                    userImage.cover(160, 240)                        
+                    
+    
+                    var mergedImage = new Jimp(FImage.bitmap.width, FImage.bitmap.height).composite(userImage, x, y ).composite(FImage, 0, 0);
+                    var file = shortid.generate() + ".png"
+                    mergedImage.write(file, function(error){
+                        if(error) { console.log(error); return;};
+                        console.log("got merged image");
+                        console.log(file);
+                        message.channel.send("**I have failed you** <@" + userID + ">", {
+                            files: [file]
+                        }).then(function(){
+                            
+                            fs.unlink(file, resultHandler);
+                        }).catch(function (err) {
+                            message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                            console.log(err.message);
+                            
+                            fs.unlink(file, resultHandler);
+                        });
+                        console.log("Message Sent");
+                    });
+                }).catch(function (err) {
+                    message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                    console.log(err.message);
+                    
+                });
+            }).catch(function (err) {
+                console.log(err.message);
+                
+            });
+        }
+    }
+}
+
+module.exports = FailCommand;
