dd3dbd383161e34022dfdbc06090410b93c3208b
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-08-21 14:40:18 +0400

    Updates

---
+++ commands/imageshit/leader.js
@@ -0,0 +1,104 @@
+const command = require("discord.js-commando");
+const Jimp = require("jimp");
+const shortid = require("shortid");
+const fs = require('fs');
+var resultHandler = function(err) { 
+    if(err) {
+       console.log("unlink failed", err);
+    } else {
+       console.log("file deleted");
+    }
+}
+var CommandCounter = require("../../index.js")
+
+class LeaderCommand extends command.Command
+ {
+    constructor(client)
+    {
+        super(client, {
+            name: "leader",
+            group: "imageshit",
+            memberName: "leader",
+            description: "Adds our leader on an image. Use the last image uploaded (required).",
+            examples: ["`!leader`"]
+        });
+    }
+
+    async run(message, args)
+    {
+        
+        CommandCounter.addCommandCounter(message.author.id)
+        var commandPrefix= "!"
+        if(message.guild != null)
+        {
+            commandPrefix = message.guild.commandPrefix
+        }
+        var url = "";
+        message.channel.fetchMessages({ around: message.id })
+        .then(messages => {
+            var messageID = "";
+            messages.filter(msg => {
+                if(msg.attachments.first() != undefined)
+                {
+                    if(msg.attachments.last().height > 0)
+                    {
+                        if(messageID == "")
+                        {
+                            messageID = msg.id;
+                            url = msg.attachments.first().url;
+                        }
+                    }
+                }
+            });
+
+            if(messageID == "")
+            {
+                message.channel.send("<@" + message.author.id + "> No image found, use `" + commandPrefix + "help leader` for help.").catch(error => {console.log("Send Error - " + error); });
+                
+                return;
+            }
+            message.channel.send("***taking image***").catch(error => {console.log("Send Error - " + error); });
+            Jimp.read("putin.png").then(function (leaderImage) {
+                console.log("got image");
+                Jimp.read(url).then(function (userImage) {
+                    
+                    userImage.cover(leaderImage.bitmap.width, leaderImage.bitmap.height)
+    
+                    var mergedImage = userImage.composite(leaderImage, 0, 0);
+                    var file = shortid.generate() + ".png"
+                    mergedImage.write(file, function(error){
+                        if(error) { console.log(error); return;};
+                        console.log("got merged image");
+                        console.log(file);
+                        message.channel.send("***Our Leader***", {
+                            files: [file]
+                        }).then(function(){
+                            
+
+                            fs.unlink(file, resultHandler);
+                        }).catch(function (err) {
+                            message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                            console.log(err.message);
+                            
+                            fs.unlink(file, resultHandler);
+                        });
+                        console.log("Message Sent");
+                    });
+                }).catch(function (err) {
+                    message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                    console.log(err.message);
+                    
+                });
+            }).catch(function (err) {
+                console.log(err.message);
+                
+            });
+        }).catch(function (err) {
+            message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+            console.log(err.message);
+            
+        });
+    }
+}
+
+module.exports = LeaderCommand;
--- commands/textshit/anthem.js
+++ commands/textshit/anthem.js
@@ -9,7 +9,7 @@ class AnthemCommand extends command.Command
             name: "anthem",
             group: "textshit",
             memberName: "anthem",
-            description: "The lyrics to ***OUR*** anthem.",
+            description: "The lyrics to our motherland's anthem.",
             examples: ["`!anthem`"]
         });
     }
@@ -17,7 +17,7 @@ class AnthemCommand extends command.Command
     async run(message, args)
     {
         CommandCounter.addCommandCounter(message.author.id)
-        message.channel.send("Rossia - sviashennaia nasha derzhava, \nRossia - lubimaia nasha strana! \nMoguchaia volia, velikaia slava - \nTvoio dostoianie na vse vremena! \n\n***CHORUS***: \nSlavsia, Otechestvo nashe svobodnoe, \nBratskikh narodov soiuz vekovoi, \nPredkami dannaia mudrost' narodnaia! \nSlavsia, strana! My gordimsia toboi! \n\nOt yuzhykh morei do poliarnogo kraia \nRaskinulis nashi lesa i polia. \nOdna ty na svete! Odna ty takaia - \nKhranimaia Bogom rodnaia zemlia! \n\n***CHORUS*** \n\nShirokii prostor dlia mechty i dlia zhizni \nGriadushie nam otkryvaiut goda. \nNam silu daiot nasha vernost otchizne. \nTak bylo, tak est' i tak budet vsegda! \n\n***CHORUS***").catch(error => console.log("Send Error - " + error));
+        message.channel.send("Rossia - sviashennaia nasha derzhava, \nRossia - lubimaia nasha strana! \nMoguchaia volia, velikaia slava - \nTvoio dostoianie na vse vremena! \n\n***CHORUS***: \nSlavsia, Otechestvo nashe svobodnoe, \nBratskikh narodov soiuz vekovoi, \nPredkami dannaia mudrost' narodnaia! \nSlavsia, strana! My gordimsia toboi! \n\nOt yuzhykh morei do poliarnogo kraia \nRaskinulis nashi lesa i polia. \nOdna ty na svete! Odna ty takaia - \nKhranimaia Bogom rodnaia zemlia! \n\n***CHORUS*** \n\nShirokii prostor dlia mechty i dlia zhizni \nGriadushie nam otkryvaiut goda. \nNam silu daiot nasha vernost otchizne. \nTak bylo, tak est' i tak budet vsegda! \n\n***CHORUS***", {files: ["anthem.mp3"]}).catch(error => console.log("Send Error - " + error));
         message.channel.send(":flag_ru:").catch(error => console.log("Send Error - " + error));
     }
 }
