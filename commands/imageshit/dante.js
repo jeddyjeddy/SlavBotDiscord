204cbaefb786c7382ef29908cb073a0c83d71e1f
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-08-24 23:44:00 +0400

    Dante & Nero Command

---
+++ commands/imageshit/dante.js
@@ -0,0 +1,168 @@
+const command = require("discord.js-commando");
+const Jimp = require("jimp");
+const shortid = require("shortid");
+const fs = require('fs');
+var resultHandler = function(err) { 
+    if(err) {
+       console.log("unlink failed", err);
+    } else {
+       console.log("file deleted");
+    }
+}
+var CommandCounter = require("../../index.js")
+
+class DanteCommand extends command.Command
+ {
+    constructor(client)
+    {
+        super(client, {
+            name: "dante",
+            group: "imageshit",
+            memberName: "dante",
+            description: "***Featuring Dante from the Devil May Cry™ Series*** This command takes the last image uploaded. This command also has an optional position parameter (1-4).",
+            examples: ["`!dante`", "`!dante <position-parameter>`","`!dante 3`"]
+        });
+    }
+
+    async run(message, args)
+    {
+        
+        CommandCounter.addCommandCounter(message.author.id)
+        var commandPrefix= "!"
+        if(message.guild != null)
+        {
+            commandPrefix = message.guild.commandPrefix
+        }
+
+        var position = 0;
+        
+        if(args.length > 0)
+        {
+            if(!isNaN(args))
+            {
+                position = parseInt(args);
+
+                if(position < 1 || position > 4)
+                {
+                    message.channel.send("The position parameter must be a number from 1 to 4 (1 = Top Left, 2 = Top Right, 3 = Bottom Left, 4 = Bottom Right).").catch(function (err) {
+                        console.log(err.message);
+                        
+                    });
+                    
+                    return;
+                }
+            }
+            else
+            {
+                message.channel.send("The position parameter must be a number from 1 to 4 (1 = Top Left, 2 = Top Right, 3 = Bottom Left, 4 = Bottom Right).").catch(function (err) {
+                    console.log(err.message);
+                    
+                });
+                
+                return;
+            }
+        }
+
+        if(position == 0)
+        {
+            position = Math.floor(Math.random() * 4) + 1;
+        }
+        
+        var url = "";
+
+        message.channel.fetchMessages({ around: message.id })
+        .then(messages => {
+            var messageID = "";
+            messages.filter(msg => {
+                if(msg.attachments.first() != undefined)
+                {
+                    if(msg.attachments.last().height > 0)
+                    {
+                        if(messageID == "")
+                        {
+                            messageID = msg.id;
+                            url = msg.attachments.first().url;
+                        }
+                    }
+                }
+            }); 
+
+            if(messageID == "")
+            {
+                message.channel.send("<@" + message.author.id + "> No image found, use `" + commandPrefix + "help dante` for help.").catch(error => {console.log("Send Error - " + error); });
+                
+                return;
+            }
+            message.channel.send("***taking image***").catch(error => {console.log("Send Error - " + error); });
+            Jimp.read("dante.png").then(function (danteImage) {
+                console.log("got image");
+                Jimp.read(url).then(function (userImage) {
+                    console.log("got avatar");
+                    if(userImage.bitmap.height > userImage.bitmap.width)
+                        danteImage.resize(Jimp.AUTO, userImage.bitmap.height * 0.3);
+                    else
+                        danteImage.resize(userImage.bitmap.height * 0.3, Jimp.AUTO);
+                    
+
+                    var x = 0;
+                    var y = 0;
+    
+                    if(position == 1)
+                    {
+                        x = 0;
+                        y = 0;
+                    }
+                    else if(position == 2)
+                    {
+                        x = userImage.bitmap.width - danteImage.bitmap.width;
+                        y = 0;
+                    }
+                    else if(position == 3)
+                    {
+                        x = 0;
+                        y = userImage.bitmap.height - danteImage.bitmap.height;
+                    }
+                    else if(position == 4)
+                    {
+                        x = userImage.bitmap.width - danteImage.bitmap.width;
+                        y = userImage.bitmap.height - danteImage.bitmap.height;
+                    }
+    
+                    
+                    var mergedImage = userImage.composite(danteImage, x, y );
+                    var file = shortid.generate() + ".png"
+                    mergedImage.write(file, function(error){
+                        if(error) { console.log(error); return;};
+                        console.log("got merged image");
+                        console.log(file);
+                        message.channel.send("***Featuring Dante from the Devil May Cry™ Series***", {
+                            files: [file]
+                        }).then(function(){
+                            fs.unlink(file, resultHandler);
+                            
+                        }).catch(function (err) {
+                            message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                            console.log(err.message);
+                            
+                            fs.unlink(file, resultHandler);
+                        });
+                        console.log("Message Sent");
+                    });
+                }).catch(function (err) {
+                    message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                    console.log(err.message);
+                    
+                });
+            }).catch(function (err) {
+                console.log(err.message);
+                
+            });
+        }).catch(function (err) {
+            message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+            console.log(err.message);
+            
+        });
+    }
+}
+
+module.exports = DanteCommand;
---
+++ commands/imageshit/nero.js
@@ -0,0 +1,231 @@
+const command = require("discord.js-commando");
+const Jimp = require("jimp");
+const shortid = require("shortid");
+const fs = require('fs');
+var resultHandler = function(err) { 
+    if(err) {
+       console.log("unlink failed", err);
+    } else {
+       console.log("file deleted");
+    }
+}
+var CommandCounter = require("../../index.js")
+
+class NeroCommand extends command.Command
+ {
+    constructor(client)
+    {
+        super(client, {
+            name: "nero",
+            group: "imageshit",
+            memberName: "nero",
+            description: "Have Nero hold an image. This command takes the last image uploaded, your avatar or the avatar of the user you have mentioned after the command.",
+            examples: ["`!nero`", "`!nero avatar`", "`!nero @User`"]
+        });
+    }
+
+    async run(message, args)
+    {
+        
+        CommandCounter.addCommandCounter(message.author.id)
+
+        var otherUser = false;
+        var userID = "";
+        var commandPrefix= "!"
+        if(message.guild != null)
+        {
+            commandPrefix = message.guild.commandPrefix
+        }
+        if(args.length > 0)
+        {
+            console.log("args are present");
+            var getUser = false;
+            for(var i = 0; i < args.length; i++)
+            {
+                if(getUser)
+                {
+                    if(args[i].toString() == ">")
+                    {
+                        i = args.length;
+                        otherUser = true;
+                    }
+                    else
+                    {
+                        if(args[i].toString() != "@" && !isNaN(args[i].toString()))
+                        {
+                            userID = userID + args[i].toString();
+                        }
+                    }
+                }
+                else
+                {
+                    if(args[i].toString() == "<")
+                    {
+                         getUser = true;
+                    } 
+                }
+            }
+        }
+        
+        var url = "";
+        console.log(url);
+
+        if(args.toString().toLowerCase() != "avatar" && !otherUser)
+        {
+            message.channel.fetchMessages({ around: message.id })
+            .then(messages => {
+                var messageID = "";
+                messages.filter(msg => {
+                    if(msg.attachments.first() != undefined)
+                    {
+                        if(msg.attachments.last().height > 0)
+                        {
+                            if(messageID == "")
+                            {
+                                messageID = msg.id;
+                                url = msg.attachments.first().url;
+                            }
+                        }
+                    }
+                });
+
+                if(messageID == "")
+                {
+                    message.channel.send("<@" + message.author.id + "> No image found, , use `" + commandPrefix + "help nero` for help.").catch(error => {console.log("Send Error - " + error); });
+                    
+                    return;
+                }
+                message.channel.send("***taking image***").catch(error => {console.log("Send Error - " + error); });
+                Jimp.read("nero.png").then(function (neroImage) {
+                    console.log("got image");
+                    Jimp.read(url).then(function (userImage) {
+                        console.log("got avatar");
+                        Jimp.read("nerohand.png").then(function (handImage) {
+                            console.log("got avatar");
+                            
+                            var x = 400
+                            var y = 300
+    
+                            userImage.scaleToFit(250, 250) 
+                            userImage.rotate(-15);                          
+                            
+            
+                            var mergedImage = neroImage.composite(userImage, x, y ).composite(handImage, 0, 0);
+                            var file = shortid.generate() + ".png"
+                            mergedImage.write(file, function(error){
+                                if(error) { console.log(error); return;};
+                                console.log("got merged image");
+                                console.log(file);
+                                message.channel.send("***This pulls my devil trigger***", {
+                                    files: [file]
+                                }).then(function(){
+                                    
+                                    fs.unlink(file, resultHandler);
+                                }).catch(function (err) {
+                                    message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                                    console.log(err.message);
+                                    
+                                    fs.unlink(file, resultHandler);
+                                });
+                                console.log("Message Sent");
+                            });
+                        }).catch(function (err) {
+                            message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                            console.log(err.message);
+                        });
+                    }).catch(function (err) {
+                        message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                        console.log(err.message);
+                    });
+                }).catch(function (err) {
+                    console.log(err.message);
+                    
+                });
+            }).catch(function (err) {
+                message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                console.log(err.message);
+                
+            });
+        }
+        else if(args.toString().toLowerCase() == "avatar" || otherUser)
+        {
+            if(otherUser)
+            {
+                console.log("other nero");
+                console.log(userID);
+    
+                message.channel.client.fetchUser(userID)
+                 .then(user => {
+                     if(user.avatarURL != undefined && user.avatarURL != null)
+                        url = user.avatarURL;
+                    else
+                        url = "no user"
+                 }, rejection => {
+                        console.log(rejection.message);
+                        url = "no user";
+                 });
+            }
+            else
+            {
+                console.log("self nero");
+                userID = message.author.id;
+                url = message.author.avatarURL;
+            }
+            Jimp.read("nero.png").then(function (neroImage) {
+                console.log("got image");
+                
+                Jimp.read(url).then(function (userImage) {
+                    console.log("got avatar");
+                    
+                    Jimp.read("nerohand.png").then(function (handImage) {
+                        console.log("got avatar");
+                        
+                        var x = 400
+                        var y = 300
+
+                        userImage.scaleToFit(250, 250) 
+                        userImage.rotate(-15);                          
+                        
+        
+                        var mergedImage = neroImage.composite(userImage, x, y ).composite(handImage, 0, 0);
+                        var file = shortid.generate() + ".png"
+                        mergedImage.write(file, function(error){
+                            if(error) { console.log(error); return;};
+                            console.log("got merged image");
+                            console.log(file);
+                            message.channel.send("***This pulls my devil trigger***", {
+                                files: [file]
+                            }).then(function(){
+                                
+                                fs.unlink(file, resultHandler);
+                            }).catch(function (err) {
+                                message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                                console.log(err.message);
+                                
+                                fs.unlink(file, resultHandler);
+                            });
+                            console.log("Message Sent");
+                        });
+                    }).catch(function (err) {
+                        message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                        console.log(err.message);
+                    });
+                }).catch(function (err) {
+                    if(url == "no user")
+                    {
+                        message.channel.send("<@" + message.author.id + "> No avatar found.").catch(error => {console.log("Send Error - " + error); });
+                    }
+                    else
+                        message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                    console.log(err.message);
+                    
+                });
+            }).catch(function (err) {
+                console.log(err.message);
+                
+            });
+        }
+    }
+}
+
+module.exports = NeroCommand;
