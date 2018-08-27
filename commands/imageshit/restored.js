cbf7aeb1397d415804677036ab607c74ac96d6d6
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-08-27 18:28:05 +0400

    Restored Command

---
+++ commands/imageshit/restored.js
@@ -0,0 +1,209 @@
+const command = require("discord.js-commando");
+const Jimp = require("jimp");
+const shortid = require("shortid");
+const fs = require('fs');
+var resultHandler = function(err) { 
+    if(err) {
+       console.log("unlink failed", err);
+    } else {
+       console.log("file deleted");
+    }
+}
+var CommandCounter = require("../../index.js")
+
+class RestoredCommand extends command.Command
+ {
+    constructor(client)
+    {
+        super(client, {
+            name: "restored",
+            group: "imageshit",
+            memberName: "restored",
+            description: "***His smile and optimism: RESTORED.*** This uses the last image uploaded, your avatar or the avatar of the user you mentioned after the command.",
+            examples: ["`!restored`", "`!restored avatar`", "`!restored @User`"]
+        });
+    }
+
+    async run(message, args)
+    {
+        
+        CommandCounter.addCommandCounter(message.author.id)
+        var otherUser = false;
+        var userID = "";
+        var commandPrefix= "!"
+        if(message.guild != null)
+        {
+            commandPrefix = message.guild.commandPrefix
+        }
+        if(args.length > 0)
+        {
+            console.log("args are present");
+            var getUser = false;
+            for(var i = 0; i < args.length; i++)
+            {
+                if(getUser)
+                {
+                    if(args[i].toString() == ">")
+                    {
+                        i = args.length;
+                        otherUser = true;
+                    }
+                    else
+                    {
+                        if(args[i].toString() != "@" && !isNaN(args[i].toString()))
+                        {
+                            userID = userID + args[i].toString();
+                        }
+                    }
+                }
+                else
+                {
+                    if(args[i].toString() == "<")
+                    {
+                         getUser = true;
+                    } 
+                }
+            }
+        }
+        
+        var url = "";
+        console.log("restored");
+        console.log(url);
+
+
+        if(args.toString().toLowerCase() != "avatar" && !otherUser)
+        {
+            message.channel.fetchMessages({ around: message.id })
+            .then(messages => {
+                var messageID = "";
+                messages.filter(msg => {
+                    if(msg.attachments.first() != undefined)
+                    {
+                        if(msg.attachments.last().height > 0)
+                        {
+                            if(messageID == "")
+                            {
+                                messageID = msg.id;
+                                url = msg.attachments.first().url;
+                            }
+                        }
+                    }
+                });
+            
+                if(messageID == "")
+                {
+                    message.channel.send("<@" + message.author.id + "> No image found, use `" + commandPrefix + "help restored` for help.").catch(error => {console.log("Send Error - " + error); });
+                    
+                    return;
+                }
+                message.channel.send("***taking image***").catch(error => {console.log("Send Error - " + error); });
+                Jimp.read(url).then(function (userImage) {
+                    console.log("got last image for restored");
+                    Jimp.read("restored.png").then(function (restoredImage) {            
+                        restoredImage.resize(userImage.bitmap.width, Jimp.AUTO)
+                        const addHeight = userImage.bitmap.height;
+                        var mergedImage = (new Jimp(userImage.bitmap.width, userImage.bitmap.height + restoredImage.bitmap.height)).composite(userImage, 0, 0).composite(restoredImage, 0, addHeight);
+        
+                        var file = shortid.generate() + ".png"
+                        mergedImage.write(file, function(error){
+                            if(error) { console.log(error); return;};
+                            console.log(file);
+                            message.channel.send("***His smile and optimism: RESTORED****", {
+                                files: [file]
+                            }).then(function(){
+                                
+                                fs.unlink(file, resultHandler);
+                            }).catch(function (err) {
+                                message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                                console.log(err.message);
+                                
+                                fs.unlink(file, resultHandler);
+                            });
+                            console.log("Message Sent");
+                        })
+                    }).catch(function (err) {
+                        message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                        console.log(err.message);
+                    }); 
+                }).catch(function (err) {
+                    message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                    console.log(err.message);
+                }); 
+            }).catch(function (err) {
+                message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                console.log(err.message);
+                
+            });
+        }
+        else if(args.toString().toLowerCase() == "avatar" || otherUser)
+        {
+            if(otherUser)
+            {
+                console.log(userID);
+    
+                message.channel.client.fetchUser(userID)
+                 .then(user => {
+                     if(user.avatarURL != undefined && user.avatarURL != null)
+                        url = user.avatarURL;
+                    else
+                        url = "no user"
+                 }, rejection => {
+                        console.log(rejection.message);
+                        url = "no user";
+                 });
+            }
+            else
+            {
+                url = message.author.avatarURL;
+            }
+            var wait = 0;
+
+            if(otherUser)
+            wait = 500;
+
+            setTimeout(function(){
+                
+                Jimp.read(url).then(function (userImage) {
+                    console.log("got avatar");
+                    Jimp.read("restored.png").then(function (restoredImage) {            
+                        restoredImage.resize(userImage.bitmap.width, Jimp.AUTO)
+                        const addHeight = userImage.bitmap.height;
+                        var mergedImage = (new Jimp(userImage.bitmap.width, userImage.bitmap.height + restoredImage.bitmap.height)).composite(userImage, 0, 0).composite(restoredImage, 0, addHeight);
+        
+                        var file = shortid.generate() + ".png"
+                        mergedImage.write(file, function(error){
+                            if(error) { console.log(error); return;};
+                            console.log(file);
+                            message.channel.send("***His smile and optimism: RESTORED***", {
+                                files: [file]
+                            }).then(function(){
+                                
+                                fs.unlink(file, resultHandler);
+                            }).catch(function (err) {
+                                message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                                console.log(err.message);
+                                
+                                fs.unlink(file, resultHandler);
+                            });
+                            console.log("Message Sent");
+                        })
+                    }).catch(function (err) {
+                        message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                        console.log(err.message);
+                    }); 
+                }).catch(function (err) {
+                    if(url == "no user")
+                    {
+                        message.channel.send("<@" + message.author.id + "> No avatar found.").catch(error => {console.log("Send Error - " + error); });
+                    }
+                    else
+                        message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                    console.log(err.message);
+                    
+                });     
+            }, wait);
+        }
+    }
+}
+
+module.exports = RestoredCommand;
