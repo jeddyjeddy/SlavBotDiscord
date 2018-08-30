d2b1e68199fbd4eea8afdb16635cbd2e19e8d858
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-08-30 17:18:33 +0400

    Fix

--- commands/imageshit/avatar.js
+++ commands/imageshit/avatar.js
@@ -1,5 +1,15 @@
 const command = require("discord.js-commando");
 var CommandCounter = require("../../index.js")
+const Jimp = require("jimp");
+const shortid = require("shortid");
+const fs = require('fs');
+var resultHandler = function(err) { 
+    if(err) {
+       console.log("unlink failed", err);
+    } else {
+       console.log("file deleted");
+    }
+}
 
 class Avatarommand extends command.Command
  {
@@ -78,12 +88,39 @@ class Avatarommand extends command.Command
         }
         else
         {
+            userID = message.author.id;
             url = message.author.avatarURL;
         }
 
         const ID = userID;
         Promise.all(promises).then(() => {
-            message.channel.send("Avatar for <@" + ID + ">", {files: [url]}).catch(error => console.log("Send Error - " + error));
+            Jimp.read(url).then(function (userImage) {
+                console.log("got avatar");
+                var file = shortid.generate() + ".png"
+                userImage.write(file, function(error){
+                    if(error) { console.log(error); return;};
+                    console.log(file);
+                    message.channel.send("Avatar for <@" + ID + ">", {
+                        files: [file]
+                    }).then(function(){
+                        fs.unlink(file, resultHandler);
+                    }).catch(function (err) {
+                        message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                        console.log(err.message);
+                        fs.unlink(file, resultHandler);
+                    });
+                    console.log("Message Sent");
+                });
+            }).catch(function (err) {
+                if(url == "no user")
+                {
+                    message.channel.send("<@" + message.author.id + "> No avatar found.").catch(error => {console.log("Send Error - " + error); });
+                }
+                else
+                    message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                console.log(err.message);
+                
+            });
         }).catch((e) => {
             console.log("User Data Error - " + e.message);
             message.channel.send("User data not found").catch(error => console.log("Send Error - " + error));
