f6dd5e1d384d0bf013827fe2f4442f9e98cbfe6d
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-08-30 19:53:56 +0400

    Bigmoji Command

--- commands/imageshit/bigmoji.js
+++ commands/imageshit/bigmoji.js
@@ -9,7 +9,7 @@ class Bigmojiommand extends command.Command
             name: "bigmoji",
             group: "imageshit",
             memberName: "bigmoji",
-            description: "Gives a high res version of an emoji from any discord server the bot is on.",
+            description: "Gives a high res version of a custom emoji from any discord server the bot is on.",
             examples: ["`!bigmoji <emoji>`"]
         });
     }
@@ -17,7 +17,90 @@ class Bigmojiommand extends command.Command
     async run(message, args)
     {
         CommandCounter.addCommandCounter(message.author.id)
-        console.log(args);
+        
+        var emojiID = "";
+        
+        if(args.length > 0)
+        {
+            console.log("args are present");
+            var getEmoji = false;
+            var waitForEnd = false;
+            for(var i = 0; i < args.length; i++)
+            {
+                if(getEmoji)
+                {
+                    if(args[i].toString() == ">")
+                    {
+                        i = args.length;
+                    }
+                    else
+                    {
+                        if(waitForEnd)
+                        {
+                            if(!isNaN(args[i].toString()))
+                            {
+                                userID = userID + args[i].toString();
+                            }
+                        }
+                        else
+                        {
+                            if(args[i].toString() == ":")
+                            {
+                                waitForEnd = true;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if(args[i].toString() == "<")
+                    {
+                        if(args.length > i + 1)
+                        {
+                            if(args[i + 1].toString() == ":")
+                            {
+                                getEmoji = true;
+                                i = i++;
+                            }
+                        }
+                    } 
+                }
+            }
+
+            if(emojiID == "")
+            {
+                message.channel.send("<@" + message.author.id + "> No custom emoji found, use `" + commandPrefix + "help bigmoji` for help.").catch(error => {console.log("Send Error - " + error); });
+                return;
+            }
+            else
+            {
+                var emojis = message.client.emojis.array()
+                var emoji = undefined;
+
+                for(var i = 0; i < emojis.length; i++)
+                {
+                    if(emojis[i].id == emojiID)
+                    {
+                        emoji = emojis[i];
+                    }
+                }
+
+                if(emoji == undefined)
+                {
+                    message.channel.send("<@" + message.author.id + "> The given emoji was not found on any of the servers this bot is on, use `" + commandPrefix + "help bigmoji` for help.").catch(error => {console.log("Send Error - " + error); });
+                    return;
+                }
+                else
+                {
+                    message.channel.send('${emoji}\n' + emoji.url).catch(error => {console.log("Send Error - " + error); });
+                }
+            }
+        }
+        else
+        {
+            message.channel.send("<@" + message.author.id + "> No custom emoji given, use `" + commandPrefix + "help bigmoji` for help.").catch(error => {console.log("Send Error - " + error); });
+            return;
+        }
     }
     
 }
