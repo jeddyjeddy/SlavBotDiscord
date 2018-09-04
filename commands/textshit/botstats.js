68fb351bbc538d3ad1aea5113df088bf46dad54b
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-09-04 21:15:47 +0400

    Bot Stats Command

---
+++ commands/textshit/botstats.js
@@ -0,0 +1,25 @@
+const command = require("discord.js-commando");
+const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+class BotStatsCommand extends command.Command
+ {
+    constructor(client)
+    {
+        super(client, {
+            name: "botstats",
+            group: "util",
+            memberName: "botstats",
+            description: "Shows the number of servers Slav Bot is on.",
+            examples: ["`!botstats`"]
+        });
+    }
+
+    async run(message, args)
+    {
+        message.channel.send("<@" + message.author.id + "> Slav Bot is currently on " + numberWithCommas(message.client.guilds.size) + " servers.").catch(error => console.log("Send Error - " + error));
+    }
+}
+
+module.exports = BotStatsCommand;
