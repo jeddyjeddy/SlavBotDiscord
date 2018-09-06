30f4bd76bc377996db42cd16618e2fbdb35859cc
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-09-06 06:23:25 +0400

    Painting and Classic Command

---
+++ commands/imageshit/classic.js
@@ -0,0 +1,85 @@
+const command = require("discord.js-commando");
+const Jimp = require("jimp");
+const shortid = require("shortid");
+const fs = require('fs');
+var resultHandler = function(err) { 
+    if(err) {
+       console.log("unlink failed", err);
+    } else {
+       console.log("file deleted");
+    }
+}
+var CommandCounter = require("../../index.js")
+
+class ClassicCommand extends command.Command
+ {
+    constructor(client)
+    {
+        super(client, {
+            name: "classic",
+            group: "imageshit",
+            memberName: "classic",
+            description: "Classic weeb shit. This command only has a text parameter.",
+            examples: ["`!classic <text>`", "`!classic this command is the ultimate form of degeneracy`"]
+        });
+    }
+
+    async run(message, args)
+    {
+        
+        CommandCounter.addCommandCounter(message.author.id)
+
+        var commandPrefix= "!"
+        if(message.guild != null)
+        {
+            commandPrefix = message.guild.commandPrefix
+        }        
+
+        if(args.length > 0 && args.length <= 135)
+        {
+            var file = shortid.generate() + ".png";
+            
+                Jimp.read("classic.jpg").then(function (trumpImage) {
+                    Jimp.loadFont("Arial_24.fnt").then(function (font) {
+                        var textWidth = 480;
+                        var textHeight = 405;
+                        var textRot = -3;
+                        var textX = 305;
+                        var textY = 645;
+
+                        var textImage = new Jimp(textWidth, textHeight);
+                        textImage.print(font, 0, 0, args.toString(), textWidth);
+                        textImage.rotate(textRot);
+
+                        trumpImage.composite(textImage, textX, textY).write(file, function(error){  
+                            if(error) { console.log(error); return;};
+                        message.channel.send("***Classic***", {
+                                    files: [file]
+                        }).then(function(){
+                            
+                            fs.unlink(file, resultHandler);
+                        }).catch(function (err) {
+                            message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                            console.log(err.message);
+                            
+                            fs.unlink(file, resultHandler);
+                        });
+                            });
+                    });
+                 }).catch(function (err) {
+                    console.log(err.message);
+                    
+                });
+        }
+        else
+        {
+            if(args.length > 0)
+             message.channel.send("<@" + message.author.id + "> Character limit for the text parameter is 135 characters, use `" + commandPrefix + "help classic` for help.").catch(error => {console.log("Send Error - " + error); });
+            else
+             message.channel.send("<@" + message.author.id + "> Text not given, use `" + commandPrefix + "help classic` for help.").catch(error => {console.log("Send Error - " + error); });
+            
+        }
+    }
+}
+
+module.exports = ClassicCommand;
---
+++ commands/imageshit/painting.js
@@ -0,0 +1,233 @@
+const command = require("discord.js-commando");
+const Jimp = require("jimp");
+const shortid = require("shortid");
+const fs = require('fs');
+var resultHandler = function(err) { 
+    if(err) {
+       console.log("unlink failed", err);
+    } else {
+       console.log("file deleted");
+    }
+}
+var CommandCounter = require("../../index.js")
+
+class PaintingCommand extends command.Command
+ {
+    constructor(client)
+    {
+        super(client, {
+            name: "painting",
+            group: "imageshit",
+            memberName: "painting",
+            description: "***I do not like this painting, Charlie. Its smug aura mocks me.*** This command takes the last image uploaded, your avatar or the avatar of the user you have mentioned after the command.",
+            examples: ["`!painting`", "`!painting avatar`", "`!painting @User`"]
+        });
+    }
+
+    async run(message, args)
+    {
+        
+        CommandCounter.addCommandCounter(message.author.id)
+
+        var otherUser = false;
+        var userID = "";
+        var commandPrefix= "!"
+        if(message.guild != null)
+        {
+            commandPrefix = message.guild.commandPrefix
+        }
+        if(args.length > 0)
+        {
+            console.log("args are present");
+            var getUser = false;
+            for(var i = 0; i < args.length; i++)
+            {
+                if(getUser)
+                {
+                    if(args[i].toString() == ">")
+                    {
+                        i = args.length;
+                        otherUser = true;
+                    }
+                    else
+                    {
+                        if(args[i].toString() != "@" && !isNaN(args[i].toString()))
+                        {
+                            userID = userID + args[i].toString();
+                        }
+                    }
+                }
+                else
+                {
+                    if(args[i].toString() == "<")
+                    {
+                         getUser = true;
+                    } 
+                }
+            }
+        }
+        
+        var url = "";
+        console.log(url);
+
+        if(args.toString().toLowerCase() != "avatar" && !otherUser)
+        {
+            message.channel.fetchMessages({ around: message.id })
+            .then(messages => {
+                var messageID = "";
+                messages.filter(msg => {
+                    if(msg.attachments.last() != undefined)
+                    {
+                        var attachments = msg.attachments.array();
+                        for(var i = attachments.length - 1; i > -1; i--)
+                        {
+                            if(attachments[i].height > 0)
+                            {
+                                if(messageID == "")
+                                {
+                                    messageID = msg.id;
+                                    url = attachments[i].url;
+                                }
+                            }
+                        }
+                    }
+                });
+
+                if(messageID == "")
+                {
+                    message.channel.send("<@" + message.author.id + "> No image found, , use `" + commandPrefix + "help painting` for help.").catch(error => {console.log("Send Error - " + error); });
+                    
+                    return;
+                }
+                message.channel.send("***taking image***").catch(error => {console.log("Send Error - " + error); });
+                Jimp.read("painting.png").then(function (paintingImage) {
+                    console.log("got image");
+                    Jimp.read(url).then(function (userImage) {
+                        console.log("got avatar");
+                        
+                        var x = 90
+                        var y = 10
+
+                        userImage.cover(190, 310);
+                        userImage.rotate(-1);
+                        
+                        var blank = new Jimp(paintingImage.bitmap.width, paintingImage.bitmap.height);
+                        blank.composite(userImage, x, y)        
+        
+                        var mergedImage = blank.composite(paintingImage, 0, 0);
+                        var file = shortid.generate() + ".png"
+                        mergedImage.write(file, function(error){
+                            if(error) { console.log(error); return;};
+                            console.log("got merged image");
+                            console.log(file);
+                            message.channel.send("***I do not like this painting, Charlie. Its smug aura mocks me.***", {
+                                files: [file]
+                            }).then(function(){
+                                
+                                fs.unlink(file, resultHandler);
+                            }).catch(function (err) {
+                                message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                                console.log(err.message);
+                                
+                                fs.unlink(file, resultHandler);
+                            });
+                            console.log("Message Sent");
+                        });
+                    }).catch(function (err) {
+                        message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                        console.log(err.message);
+                        
+                    });
+                }).catch(function (err) {
+                    console.log(err.message);
+                    
+                });
+            }).catch(function (err) {
+                message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                console.log(err.message);
+                
+            });
+        }
+        else if(args.toString().toLowerCase() == "avatar" || otherUser)
+        {
+            var promises = []
+            if(otherUser)
+            {
+                console.log("other painting");
+                console.log(userID);
+    
+                promises.push(message.channel.client.fetchUser(userID)
+                .then(user => {
+                    if(user.avatarURL != undefined && user.avatarURL != null)
+                       url = user.avatarURL;
+                   else
+                       url = "no user"
+                }, rejection => {
+                       console.log(rejection.message);
+                       url = "no user";
+                }))
+            }
+            else
+            {
+                console.log("self painting");
+                userID = message.author.id;
+                url = message.author.avatarURL;
+            }
+
+            Promise.all(promises).then(() => {
+    Jimp.read("painting.png").then(function (paintingImage) {
+                    console.log("got image");
+                    
+                    Jimp.read(url).then(function (userImage) {
+                        console.log("got avatar");    
+                        var x = 90
+                        var y = 10
+
+                        userImage.cover(190, 310);
+                        userImage.rotate(-1);
+
+                        var blank = new Jimp(paintingImage.bitmap.width, paintingImage.bitmap.height);
+                        blank.composite(userImage, x, y)        
+        
+                        var mergedImage = blank.composite(paintingImage, 0, 0);
+                        var file = shortid.generate() + ".png"
+                        mergedImage.write(file, function(error){
+                            if(error) { console.log(error); return;};
+                            console.log("got merged image");
+                            console.log(file);
+                            message.channel.send("***I do not like this painting, Charlie. Its smug aura mocks me.***", {
+                                files: [file]
+                            }).then(function(){
+                                
+                                fs.unlink(file, resultHandler);
+                            }).catch(function (err) {
+                                message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                                console.log(err.message);
+                                
+                                fs.unlink(file, resultHandler);
+                            });
+                            console.log("Message Sent");
+                        });
+                    }).catch(function (err) {
+                        if(url == "no user")
+                        {
+                            message.channel.send("<@" + message.author.id + "> No avatar found.").catch(error => {console.log("Send Error - " + error); });
+                        }
+                        else
+                            message.channel.send("Error - " + err.message).catch(error => {console.log("Send Error - " + error); });
+                        console.log(err.message);
+                        
+                    });
+                }).catch(function (err) {
+                    console.log(err.message);
+                    
+                });
+            }).catch((e) => {
+                console.log("User Data Error - " + e.message);
+                message.channel.send("User data not found").catch(error => console.log("Send Error - " + error));
+            });
+        }
+    }
+}
+
+module.exports = PaintingCommand;
