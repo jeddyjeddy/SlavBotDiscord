3743d620d471e752d932e12d80dd26ed347ebc32
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-09-13 13:45:00 +0400

    Fix

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ dbl.on('posted', () => {
         }
     })
 
-    bot.user.setActivity('Despacito ' + numberWithCommas(Math.floor(Math.random() * 9999) + 1), { type: 'LISTENING' }).catch(console.error);
+    bot.user.setActivity('Despacito ' + numberWithCommas(Math.floor(Math.random() * 9999) + 1), { type: 'LISTENING' }).catch((error) => console.log(error));
 });
     
 dbl.on('error', e => {
