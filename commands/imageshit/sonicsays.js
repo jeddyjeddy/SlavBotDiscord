95606c67d637b84556a5f7292243f35dd30d132d
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-09-19 15:04:51 +0400

    SonicSays Fix

--- commands/imageshit/sonicsays.js
+++ commands/imageshit/sonicsays.js
@@ -28,127 +28,15 @@ class SonicsaysCommand extends command.Command
     {
         
         CommandCounter.addCommandCounter(message.author.id)
-        var editText = "";
-        var editTextExtra = "";
-        var editTextExtra2 = "";
-        var editTextExtra3 = ""
-        var editTextExtra4 = ""
-        var editTextExtra5 = ""
-        var editTextExtra6 = ""
 
         if(args.length > 0 && args.length < 141)
         {
-            var nextLine = false;
-            var nextLine2 = false;
-            var nextLine3 = false;
-            var nextLine4 = false;
-            var nextLine5 = false;
-            var nextLine6 = false;
-
-
-            for(var i = 0; i < args.length; i++)
-            {
-                if(i > 120 && args.length >= 121)
-                {
-                    if(nextLine6)
-                    {
-                        editTextExtra6 = editTextExtra6 + args[i].toString();
-                    }
-                    else
-                    {
-                        editTextExtra5 = editTextExtra5 + args[i].toString();
-                        if(args[i].toString() == " ")
-                        {
-                            nextLine6 = true;
-                        }
-                    }
-                }
-                else if(i > 100 && args.length >= 101)
-                {
-                    if(nextLine5)
-                    {
-                        editTextExtra5 = editTextExtra5 + args[i].toString();
-                    }
-                    else
-                    {
-                        editTextExtra4 = editTextExtra4 + args[i].toString();
-                        if(args[i].toString() == " ")
-                        {
-                            nextLine5 = true;
-                        }
-                    }
-                }
-                else if(i > 80 && args.length >= 81)
-                {
-                    if(nextLine4)
-                    {
-                        editTextExtra4 = editTextExtra4 + args[i].toString();
-                    }
-                    else
-                    {
-                        editTextExtra3 = editTextExtra3 + args[i].toString();
-                        if(args[i].toString() == " ")
-                        {
-                            nextLine4 = true;
-                        }
-                    }
-                }
-                else if(i > 60 && args.length >= 61)
-                {
-                    if(nextLine3)
-                    {
-                        editTextExtra3 = editTextExtra3 + args[i].toString();
-                    }
-                    else
-                    {
-                        editTextExtra2 = editTextExtra2 + args[i].toString();
-                        if(args[i].toString() == " ")
-                        {
-                            nextLine3 = true;
-                        }
-                    }
-                }
-                else if(i > 40 && args.length >= 41)
-                {
-                    if(nextLine2)
-                    {
-                        editTextExtra2 = editTextExtra2 + args[i].toString();
-                    }
-                    else
-                    {
-                        editTextExtra = editTextExtra + args[i].toString();
-                        if(args[i].toString() == " ")
-                        {
-                            nextLine2 = true;
-                        }
-                    }
-                }
-                else if(i > 20 && args.length >= 21)
-                {
-                    if(nextLine)
-                    {
-                        editTextExtra = editTextExtra + args[i].toString();
-                    }
-                    else
-                    {
-                        editText = editText + args[i].toString();
-                        if(args[i].toString() == " ")
-                        {
-                            nextLine = true;
-                        }
-                    }
-                }
-                else
-                    editText = editText + args[i].toString();
-            }
             const file = shortid.generate() + ".png";
-            console.log("1: " + editText + "\n2:" + editTextExtra + "\n3:" + editTextExtra2);
+            var editText = args.toString()
             
-                Jimp.read("sonic.jpg").then(function (sonicImage) {
-                    Jimp.loadFont(Jimp.FONT_SANS_16_WHITE).then(function (font) {
-                        sonicImage.print(font, 90, 70, editText).print(font, 90, 90, editTextExtra).print(font, 90, 110, editTextExtra2)
-                         .print(font, 90, 130, editTextExtra3).print(font, 90, 150, editTextExtra4).print(font, 90, 170, editTextExtra5)
-                         .print(font, 90, 190, editTextExtra6).write(file, function(error){ 
+                Jimp.read("sonic.png").then(function (sonicImage) {
+                    Jimp.loadFont(Jimp.FONT_SANS_32_WHITE).then(function (font) {
+                        sonicImage.print(font, 20, 115, editText, 700).write(file, function(error){ 
                             if(error) { console.log(error); return;};
                         message.channel.send("***Sonic says...***", {
                                     files: [file]
