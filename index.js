df2d2a7575e8c39f38fda61d18deaa66acfebd45
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-09-21 15:47:44 +0400

    Fix

--- index.js
+++ index.js
@@ -1156,7 +1156,7 @@ var initData = () => {
                 if(childSnap.child("disable").val() != null)
                 {
                     snapshot.forEach(function(cmdOrGrpSnap){
-                        cmdOrGroup = cmdOrGrpSnap.val();
+                        cmdOrGroup = cmdOrGrpSnap.key();
                         var cmdOrGroupRef = null;
                         var promises = []
                         promises.push(bot.registry.resolveGroup(cmdOrGroup).then(group => {
