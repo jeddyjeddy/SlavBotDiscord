c464f0242d0305a38679bb0510e5860424902a1e
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-10-07 19:08:29 +0400

    Fix

--- index.js
+++ index.js
@@ -841,16 +841,19 @@ var webhook = listener.createServer({
     request.on('data', (chunk) => {
         body.push(chunk);
       }).on('end', () => {
-        body = JSON.parse(Buffer.concat(body).toString());
-        if(body !== undefined && body !== null)
-        {
-            bot.fetchUser(body["user"]).then(user => {
-                    addUserTokens(body["user"], giveawayToken)
-                    user.send("Thank you for voting, you have recieved " + numberWithCommas(giveawayToken) + " tokens. You now have " + numberWithCommas(getUserTokens(body["user"])) + " tokens.").catch(error => console.log("Send Error - " + error));
-            }, rejection => {
-                    console.log(rejection.message);
-            });
-        }
+          if(body != [])
+          {
+            body = JSON.parse(Buffer.concat(body).toString());
+            if(body !== undefined && body !== null)
+            {
+                bot.fetchUser(body["user"]).then(user => {
+                        addUserTokens(body["user"], giveawayToken)
+                        user.send("Thank you for voting, you have recieved " + numberWithCommas(giveawayToken) + " tokens. You now have " + numberWithCommas(getUserTokens(body["user"])) + " tokens.").catch(error => console.log("Send Error - " + error));
+                }, rejection => {
+                        console.log(rejection.message);
+                });
+            }
+          }
       });
 });
 var port = 5000;
