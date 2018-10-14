72b358c31ef5e13b7ba58901b36080e547ac7b14
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-10-15 01:27:40 +0400

    Fix

--- commands/games/ww.js
+++ commands/games/ww.js
@@ -70,18 +70,13 @@ class WWCommand extends command.Command
                 }
                 else
                 {
-                    if(message.guild.id == "465522025440739328")
+                    var war = JSON.parse(snapshot.val())
+                    if(war.ended == true && war.countries != [])
                     {
-                        var war = JSON.parse(snapshot.val())
-                       // if(war.ended == true && war.countries != [])
-                       // {
-                            war.countries = []
-                            firebase.database().ref("serversettings/" + message.guild.id + "/wars").set(JSON.stringify(war))
-                       // }
-                        wars.push(war)
-                        console.log(war)
+                        war.countries = []
+                        firebase.database().ref("serversettings/" + message.guild.id + "/wars").set(JSON.stringify(war))
                     }
-                    
+                    wars.push(war)
                 }
             }))
         }
