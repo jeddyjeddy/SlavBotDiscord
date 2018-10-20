1751b1b1e274f4b0824f8f1ca2c4d1a796374ca3
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-10-20 22:48:46 +0400

    Test

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ var config = {
   var signedIntoFirebase = false;
   firebase.initializeApp(config);
   
-  firebase.auth().signInAnonymously().catch(function(error) {
+  /*firebase.auth().signInAnonymously().catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
   
       console.log(errorCode);
       console.log(errorMessage);
-  });
+  });*/
   
   firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
