b1f6aea7dce62ba175b91402c42b87ff5bd4371c
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2018-12-31 15:27:26 +0400

    Fix

--- commands/games/ww.js
+++ commands/games/ww.js
@@ -515,7 +515,6 @@ class WWCommand extends command.Command
                                     maxValue = Math.floor(2000 * ((maxPercInc/100) + 1))
 
                                     var collected = Math.floor(Math.random() * maxValue) + 1
-
                                     collected = Math.floor(collected * ((collectedValInc/100) + 1))
 
                                     var timestamp = (new Date(Date.now()).toJSON());
@@ -523,7 +522,7 @@ class WWCommand extends command.Command
                                     IndexRef.addTokens(message.author.id, collected)
                                     IndexRef.setCooldown(message.author.id, (new Date((new Date).getTime() + 120000)))
     
-                                    message.channel.send("", {embed: {title: "***Resources Collected***", description: "<@" + message.author.id + "> You have collected ***" + numberWithCommas(collected) + " tokens*** with a ***max value increase of " + maxPercInc + "%*** _(current max value: " + maxValue + ")_ and ***value increase of " + collectedValInc + "%***\n\n***Max Value Increase %*** - This % can be increased by conquering countries with a value greater than 1000 tokens. The higher the value, the higher the % increase in the maximum amount of tokens you can collect. The highest % allowed is 200%\n\n***Collected Value Increase %*** - You can increase the value of tokens you have collected by a certain %. This is only available to those ***[supporting us on Patreon](https://www.patreon.com/merriemweebster)***. 50% for low tier supporters and 100% for high tier supporters.", color: 65339, timestamp: timestamp, footer: {icon_url: message.client.user.avatarURL,text: "Collected on"}}}).catch(error => console.log("Send Error - " + error));
+                                    message.channel.send("", {embed: {title: "***Resources Collected***", description: "<@" + message.author.id + "> You have collected ***" + numberWithCommas(collected) + " tokens***\n\n***Max value increase of " + maxPercInc + "%*** _(current max value: " + maxValue + ")_ - Increase the max amount of tokens you can collect by conquering countries with a value greater than 1000 tokens.\n\n***Collected Value Increase of " + collectedValInc + "%*** - You can increase the value of tokens you have collected. This is only available to those ***[supporting us on Patreon](https://www.patreon.com/merriemweebster)***.", color: 65339, timestamp: timestamp, footer: {icon_url: message.client.user.avatarURL,text: "Collected on"}}}).catch(error => console.log("Send Error - " + error));
                                 }
                                 else
                                 {
