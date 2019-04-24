91f38c61be9316384a129f40da5f4988a41d4c1d
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2019-04-24 16:20:56 +0400

    Fix

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("Starting Bot")
 const { ShardingManager } = require('discord.js');
-const Manager = new ShardingManager('./index.js', { token: process.env.BOT_TOKEN, totalShards: 6 });
+const Manager = new ShardingManager('./index.js', { token: process.env.BOT_TOKEN, totalShards: 5 });
 Manager.spawn();
 Manager.on('launch', shard => console.log(`Successfully launched shard ${shard.id}`));
 
