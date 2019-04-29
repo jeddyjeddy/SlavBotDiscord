bbdf1861bae1a4c7f3a24b71af0d53da00a54e34
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2019-04-29 20:39:57 +0400

    Stand Battle & Triggered Command

--- commands/textshit/standgenerator.js
+++ commands/textshit/standgenerator.js
@@ -18,8 +18,8 @@ class StandGeneratorCommand extends command.Command
             name: "standgenerator",
             group: "textshit",
             memberName: "standgenerator",
-            description: "Generate a stand for yourself or another user.",
-            examples: ["`!standgenerator`", "`!standgenerator @User`"]
+            description: "Generate a stand for yourself or another user. You can also battle another user with randomly generated stands.",
+            examples: ["`!standgenerator`", "`!standgenerator @User`", "!standgenerator battle @User"]
         });
     }
 
@@ -28,6 +28,7 @@ class StandGeneratorCommand extends command.Command
         CommandCounter.addCommandCounter(message.author.id)
         var otherUser = false;
         var userID = "";
+        var battle = false;
 
         if(args.length > 0)
         {
@@ -57,6 +58,16 @@ class StandGeneratorCommand extends command.Command
                     } 
                 }
             }
+
+            if(args.toLowerCase().startsWith("battle "))
+            {
+                battle = true;
+                if(!otherUser)
+                {
+                    message.channel.send("<@" + message.author.id + "> You must tag another user for a stand battle.").catch(error => console.log("Send Error - " + error));
+                    return;
+                }
+            }
         }
         
         if(!otherUser)
@@ -64,12 +75,120 @@ class StandGeneratorCommand extends command.Command
             userID = message.author.id;
         }
 
-        const standName = uniqueNamesGenerator(' ')
-        var standText = "***『Stand User』<@" + userID + ">***\n\n***『Stand Name』" + toTitleCase(standName) + "***\n\n*Power - " + ranks[Math.floor(Math.random() * ranks.length)]
-        + "*\n\n*Speed - " + ranks[Math.floor(Math.random() * ranks.length)] + "*\n\n*Range - " + ranks[Math.floor(Math.random() * ranks.length)] + "*\n\n"
-        + "*Durability - " + ranks[Math.floor(Math.random() * ranks.length)] + "*\n\n*Precision - " + ranks[Math.floor(Math.random() * ranks.length)] + "*\n\n*Potential - " + ranks[Math.floor(Math.random() * ranks.length)] + "*"
+        if(battle)
+        {
+            var selfAbilities = []
 
-        message.channel.send(standText, {files: ["jojo.gif"]}).then(() => {message.channel.send("***" + responses[Math.floor(Math.random() * responses.length)] + "***").catch(error => console.log("Send Error - " + error));}).catch(error => console.log("Send Error - " + error));
+            for(var i = 0; i < 6; i++)
+            {
+                selfAbilities.push(ranks[Math.floor(Math.random() * ranks.length)])
+            }
+
+            var opponentAbilities = []
+
+            for(var i = 0; i < 6; i++)
+            {
+                opponentAbilities.push(ranks[Math.floor(Math.random() * ranks.length)])
+            }
+
+            var selfPoints = 0, opponentPoints = 0;
+
+            for(var i = 0; i < selfAbilities.length; i++)
+            {
+                if(selfAbilities[i] == "A")
+                {
+                    selfPoints += 5;
+                }
+                else if(selfAbilities[i] == "B")
+                {
+                    selfPoints += 4;
+                }
+                else if(selfAbilities[i] == "C")
+                {
+                    selfPoints += 3;
+                }
+                else if(selfAbilities[i] == "D")
+                {
+                    selfPoints += 2;
+                }
+                else if(selfAbilities[i] == "E")
+                {
+                    selfPoints += 1;
+                }
+            }
+
+            for(var i = 0; i < opponentAbilities.length; i++)
+            {
+                if(opponentAbilities[i] == "A")
+                {
+                    opponentPoints += 5;
+                }
+                else if(opponentAbilities[i] == "B")
+                {
+                    opponentPoints += 4;
+                }
+                else if(opponentAbilities[i] == "C")
+                {
+                    opponentPoints += 3;
+                }
+                else if(opponentAbilities[i] == "D")
+                {
+                    opponentPoints += 2;
+                }
+                else if(opponentAbilities[i] == "E")
+                {
+                    opponentPoints += 1;
+                }
+            }
+
+            var result = ""
+
+            if(selfPoints > opponentAbilities)
+            {
+                result = "***<@" + message.author.id + "> has defeated <@" + userID + "> !!!***"
+            }
+            else if (selfPoints < opponentAbilities)
+            {
+                result = "***<@" + userID + "> has defeated <@" + message.author.id + "> !!!***"
+            }
+            else
+            {
+                result = "***The battle has resulted in a tie!!!***"
+            }
+
+            const standName = uniqueNamesGenerator(' ')
+            var standTextSelf = "***『Stand User』<@" + message.author.id + ">***\n\n***『Stand Name』" + toTitleCase(standName) 
+            + "***\n\n*Power - " + selfAbilities[0]
+            + "*\n\n*Speed - " + selfAbilities[1] + "*\n\n*Range - " + selfAbilities[2] + "*\n\n"
+            + "*Durability - " + selfAbilities[3] + "*\n\n*Precision - " 
+            + selfAbilities[4] + "*\n\n*Potential - " 
+            + selfAbilities[5] + "*"
+            
+            var standTextOpponent = "***『Stand User』<@" + userID + ">***\n\n***『Stand Name』"
+             + toTitleCase(standName) + "***\n\n*Power - " + opponentAbilities[0]
+            + "*\n\n*Speed - " + opponentAbilities[1] + "*\n\n*Range - " + opponentAbilities[2] + "*\n\n"
+            + "*Durability - " + opponentAbilities[3] + "*\n\n*Precision - " + opponentAbilities[4] + "*\n\n*Potential - " 
+            + opponentAbilities[5] + "*"
+    
+            message.channel.send("*** <@" + message.author.id + "> vs <@" + userID + ">***\n\n\n").then(() => {
+                message.channel.send(standTextSelf).then(() => {
+                    message.channel.send(standTextOpponent).then(() => {
+                        message.channel.send(result, {files: ["jojo.gif"]}).then(() => {
+                            message.channel.send("***" + responses[Math.floor(Math.random() * responses.length)] + "***").catch(error => console.log("Send Error - " + error));
+                        }).catch(error => console.log("Send Error - " + error));
+                    }).catch(error => console.log("Send Error - " + error));
+                }).catch(error => console.log("Send Error - " + error));
+            }).catch(error => console.log("Send Error - " + error));
+        }
+        else
+        {
+            const standName = uniqueNamesGenerator(' ')
+            var standText = "***『Stand User』<@" + userID + ">***\n\n***『Stand Name』" + toTitleCase(standName) + "***\n\n*Power - " + ranks[Math.floor(Math.random() * ranks.length)]
+            + "*\n\n*Speed - " + ranks[Math.floor(Math.random() * ranks.length)] + "*\n\n*Range - " + ranks[Math.floor(Math.random() * ranks.length)] + "*\n\n"
+            + "*Durability - " + ranks[Math.floor(Math.random() * ranks.length)] + "*\n\n*Precision - " + ranks[Math.floor(Math.random() * ranks.length)] + "*\n\n*Potential - " + ranks[Math.floor(Math.random() * ranks.length)] + "*"
+    
+            message.channel.send(standText, {files: ["jojo.gif"]}).then(() => {message.channel.send("***" + responses[Math.floor(Math.random() * responses.length)] + "***").catch(error => console.log("Send Error - " + error));}).catch(error => console.log("Send Error - " + error));
+        }
     }
 }
 
