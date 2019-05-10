2131be53617a03bcb89867d838e0dc058ebfa213
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2019-05-11 02:54:26 +0400

    Fix

--- index.js
+++ index.js
@@ -1126,18 +1126,29 @@ bot.on("guildMemberUpdate", (oldMemberData, newMemberData) => {
                             }
                         }
 
-                        DatabaseFunctions.addUserTokens(newMemberData.user.id, 100000)
                         const roleName = newRoles[i].name
-                        setTimeout(() => {
-                            if(premiumSupporter)
-                            {
-                                DatabaseFunctions.addUserTokens(newMemberData.user.id, 500000)
-                                newMemberData.user.send("Thank you for supporting Slav Bot! You have been given the ***" + roleName + "*** role and ***" + premiumRoleName + "***. Your name should be added on the *hall-of-gopniks* channel in Slav Support. If that is not the case, then please inform an Admin or the Owner on Slav Support. You have also been given 100k War Tokens for the World War and War Slave games and will receive this every month as long as you continue to be a patron. You have also received a one time payment of 500k War Tokens for opting for our highest tier.").catch(error => console.log("Send Error - " + error));
-                            }
-                            else
-                                newMemberData.user.send("Thank you for supporting Slav Bot! You have been given the ***" + roleName + "*** role. Your name should be added on the *hall-of-gopniks* channel in Slav Support. If that is not the case, then please inform an Admin or the Owner on Slav Support. You have also been given 100k War Tokens for the World War and War Slave games and will receive this every month as long as you continue to be a patron.").catch(error => console.log("Send Error - " + error));
-                        }, 500)
 
+                        if(added)
+                        {
+                            if(premiumSupporter)
+                                newMemberData.user.send("Welcome Back! Your benefits for ***" + premiumRoleName + "*** tier have been added back.").catch(error => console.log("Send Error - " + error));
+                            else
+                                newMemberData.user.send("Welcome Back! Your benefits for ***" + roleName + "*** tier have been added back.").catch(error => console.log("Send Error - " + error));
+                        }
+                        else
+                        {
+                            DatabaseFunctions.addUserTokens(newMemberData.user.id, 100000)
+                            setTimeout(() => {
+                                if(premiumSupporter)
+                                {
+                                    DatabaseFunctions.addUserTokens(newMemberData.user.id, 500000)
+                                    newMemberData.user.send("Thank you for supporting Slav Bot! You have been given the ***" + roleName + "*** role and ***" + premiumRoleName + "***. Your name should be added on the *hall-of-gopniks* channel in Slav Support. If that is not the case, then please inform an Admin or the Owner on Slav Support. You have also been given 100k War Tokens for the World War and War Slave games and will receive this every month as long as you continue to be a patron. You have also received a one time payment of 500k War Tokens for opting for our highest tier.\n\nLeaving the support server means being unable to receive these benefits, you must join back to regain them.").catch(error => console.log("Send Error - " + error));
+                                }
+                                else
+                                    newMemberData.user.send("Thank you for supporting Slav Bot! You have been given the ***" + roleName + "*** role. Your name should be added on the *hall-of-gopniks* channel in Slav Support. If that is not the case, then please inform an Admin or the Owner on Slav Support. You have also been given 100k War Tokens for the World War and War Slave games and will receive this every month as long as you continue to be a patron.\n\nLeaving the support server means being unable to receive these benefits, you must join back to regain them.").catch(error => console.log("Send Error - " + error));
+                            }, 500)
+                        }
+                     
                         firebase.database().ref("patrons/" + newMemberData.id).set(1)
                     }
                 }
@@ -1189,12 +1200,20 @@ bot.on("guildMemberUpdate", (oldMemberData, newMemberData) => {
                             }
                         }
 
-                        DatabaseFunctions.addUserTokens(newMemberData.user.id, 50000)
                         const roleName = newRoles[i].name
-                        setTimeout(() => {
-                            newMemberData.user.send("Thank you for supporting Slav Bot! You have been given the ***" + roleName + "*** role. Your name should be added on the *hall-of-slavs* channel in Slav Support. You have also been given 50k War Tokens for the World War and War Slave games and will receive this every month as long as you continue to be a patron.").catch(error => console.log("Send Error - " + error));
-                        }, 500)
 
+                        if(added)
+                        {
+                            newMemberData.user.send("Welcome Back! Your benefits for ***" + roleName + "*** tier have been added back.").catch(error => console.log("Send Error - " + error));
+                        }
+                        else
+                        {
+                            DatabaseFunctions.addUserTokens(newMemberData.user.id, 50000)
+                            setTimeout(() => {
+                                newMemberData.user.send("Thank you for supporting Slav Bot! You have been given the ***" + roleName + "*** role. Your name should be added on the *hall-of-slavs* channel in Slav Support. You have also been given 50k War Tokens for the World War and War Slave games and will receive this every month as long as you continue to be a patron.\n\nLeaving the support server means being unable to receive these benefits, you must join back to regain them.").catch(error => console.log("Send Error - " + error));
+                            }, 500)
+                        }
+                        
                         firebase.database().ref("patrons/" + newMemberData.id).set(0)
                     }
                 }
@@ -1725,7 +1744,7 @@ bot.on("guildMemberRemove", (member) => {
     var channelID;
 
     if(member.guild.id == supportServerID)
-    {
+    {        
         firebase.database().ref("patrons/" + member.id).remove()
     }
 
