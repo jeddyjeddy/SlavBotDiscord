bcdb0dbbc0fe7d00cdd1ebe6875d582d01597baf
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2019-05-10 16:12:03 +0400

    Fix

--- main.js
+++ main.js
@@ -14,5 +14,31 @@ Manager.on("message", (shard, message) => {
                 shardToUse.eval(`this.fetchUser(${data["user"]}).then(user => {user.send("Thank you for voting, you have received " + ${data["token1"]} + " tokens. You now have " + ${data["token2"]} + " tokens. You can now use the \`dailyspin\` command. Use \`help ww\` for more info on these tokens and \`help dailyspin\` for info on Daily Spins.").catch(error => console.log("Send Error - " + error));}, rejection => {this.shard.send(${JSON.stringify(data)});});`)
             }
         }
+        else if(message.toString().indexOf("respawnAlert") > -1)
+        {
+            var data = JSON.parse(message)
+            if(data["respawnAlert"] == "all")
+            {
+                if(shardToUse.id == shard.id + 1)
+                {
+                    Manager.respawnAll()
+                }
+            }
+            else if(data["respawnAlert"] == "few")
+            {
+                if(data["amount"] != null && data["amount"] != undefined)
+                {
+                    var amount = data["amount"]
+                    for(var i = 0; i < amount.length; i++)
+                    {
+                        if(amount[i] == shardToUse.id)
+                        {
+                            shardToUse.respawn()
+                        }
+                    }
+                }
+            }
+            
+        }
     })
 })
