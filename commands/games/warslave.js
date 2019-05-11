b135d4dc3e57ab52125b0e784f1b3e15ff0371a5
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2019-05-12 02:33:12 +0400

    Fix

--- commands/games/warslave.js
+++ commands/games/warslave.js
@@ -117,7 +117,7 @@ class WarSlaveCommand extends command.Command
                                 }
                             }
 
-                            if(((slave.users[slaveIndex].id).toString()).contains("&"))
+                            if(((slave.users[slaveIndex].id).toString()).indexOf("&") > -1)
                             {
                                 slave.users.splice(slaveIndex, 1)
                                 copies = true;
