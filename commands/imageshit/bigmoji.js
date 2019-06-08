f425f7584995feb68ff155faf9faf163a8326261
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2019-06-08 16:06:31 +0400

    Fix

--- commands/imageshit/bigmoji.js
+++ commands/imageshit/bigmoji.js
@@ -87,7 +87,7 @@ class Bigmojiommand extends command.Command
             }
             else
             {
-                message.client.shard.broadcastEval('function emojis(){return this.emojis.array()} return emojis()')
+                message.client.shard.broadcastEval('bot.emojis.array()')
                 .then(results => {
                     console.log(results)
                     var emojis = []
